d643eb09771cbc12e91e9c66de82be8d265e38b9
julzdoug <jschwarzzh@hotmail.com>
2023-02-10 18:00:05 +0100

    update index question working

--- questions.php
+++ questions.php
@@ -1,114 +1,258 @@
 <?php
-include "./scripts/php_includes/data-collector.php";
+   include "./scripts/php_includes/data-collector.php";
 ?>
 <!doctype html>
 <html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.104.2">
+    <title>Sticky Footer Navbar Template · Bootstrap v5.2</title>
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-  <meta name="generator" content="Hugo 0.104.2">
-  <title>Sticky Footer Navbar Template · Bootstrap v5.2</title>
-  <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/sticky-footer-navbar/">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <!-- Favicons -->
-  <link rel="apple-touch-icon" href="/docs/5.2/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-  <link rel="icon" href="/docs/5.2/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-  <link rel="icon" href="/docs/5.2/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-  <link rel="manifest" href="/docs/5.2/assets/img/favicons/manifest.json">
-  <link rel="mask-icon" href="/docs/5.2/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
-  <link rel="icon" href="/docs/5.2/assets/img/favicons/favicon.ico">
-  <meta name="theme-color" content="#712cf9">
-  <!-- Custom styles for this template -->
-  <link href="css/sticky-footer-navbar.css" rel="stylesheet">
-</head>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/sticky-footer-navbar/">
 
-<body class="d-flex flex-column h-100 bg-info">
+    
+
+    
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/5.2/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/5.2/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/5.2/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/5.2/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/5.2/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
+<link rel="icon" href="/docs/5.2/assets/img/favicons/favicon.ico">
+<meta name="theme-color" content="#712cf9">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="css/sticky-footer-navbar.css" rel="stylesheet">
+  </head>
+  <body class="d-flex flex-column h-100 bg-info">
+    
+<header>
+<?php 
+
+
+if (isset($quiz["questionIdSequence"])) {
+  $questionCount = $quiz["questionNum"];
+  $id = $quiz["questionIdSequence"][$currentQuestionIndex];
+}
+
+
+// Frage auslesen
+
+$question = fetchQuestionById($id, $dbConn);
+    
+    ?>
+  <!-- Fixed navbar -->
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><h5 class="mt-0 text-light">Restart Trivia Quiz</h5></a>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      </div>
+    </div>
+  </nav>
+</header>
+
+
+
+<!-- Begin page content -->
+
+
+<main class="flex-shrink-0">
+
+  <div class="container-fluid">
+  <div class="col">
+    <h1 class="mt-5"></h1>
+    </div>
+    <div class="col">
+    <h2 class="mt-5"></h1>
+    </div>
+    <div class="col">
+     
+    </div>
+  </div>
+
+  <div class="col">
+    
+    </div>
+    <div class="col">
+    
+    </div>
+    <div class="col">
+    </div>
+    <div class="row">
+
+  
+    <p class="lead"></p>
+    <p><h6> Question <?php echo ($currentQuestionIndex +1)
+?> of <?php echo $quiz["questionNum"];?> : <?php echo $question["question_text"]; ?> </h6></p>
+
+<p><h7>Your answer:</h7></p>
+    </div>
+
+  <form action="<?php echo $link; ?>" method="post">
+
   <?php
 
-$currentIndex = 0; 
-$currentQuestionId = 1; 
+/* $correct = $question["is_correct"];
+
+  for ($a = 1; $a <= 5; $a++) {
+
+    $answerColName = "answer-" . $a;
 
 
-// prepare and execute the select statement to select a question
-$selectQuestion = $dbConn->prepare("select * from questions where id = ?");
-$selectQuestion->bindValue(1, $currentQuestionId); 
-$selectQuestion->execute(); 
-$currentQuestion = $selectQuestion->fetch(PDO::FETCH_ASSOC); 
+    
 
-/*
-print "<pre>"; 
-print_r($currentQuestion); 
-print "</pre>"; 
-*/
+    if(isset($question[$answerColName])&&$question[$answerColName] !== ''){
+      $answerText = $question[$answerColName];
+    if ($correct === $answerColName) $value = 1;
+    else $value = 0;
 
-echo '<div class="container-fluid p-5">';
-echo '<div class="container justify-content-center">';
-echo  '<div class="row">';
-    echo '<form class="md col w-50 mt-5">'; 
-        echo "<div class=h3>Question " . $currentIndex + 1 . "</div>";
-        echo "<div class=h4>$currentQuestion[question_text]</div>";
-        echo '<img src="/images/' . $currentQuestion["image"] . '" width="400px"></img>';                         
+    echo "<div class='form-check form-check-inline'>
+  
+    <input class='form-check-input' type='radio' name='single-choice' id= '$answerColName' value='$value'>
+    <label class='form-check-label' for='single-choice-0'>$answerText</label>
+  </div>";
+  }
+}; */
 
-        // prepare and execute the select statements
-        $selectAnswers = $dbConn->prepare("select * from answers where question_id = ?");
-        $selectAnswers->bindValue(1, $currentQuestion["id"]);                 
-        $selectAnswers->execute();                 
+   // prepare and execute the select statements
+   $selectAnswers = $dbConn->prepare("select * from answers where question_id = ?");
+   $selectAnswers->bindValue(1, $question["id"]);                 
+   $selectAnswers->execute();                 
 
-        if ($currentQuestion["type"] == "MULTIPLE") {
-            
-            // display checkboxes buttons for answers to questions with MULTIPLE answers
-            while ($answer = $selectAnswers->fetch(PDO::FETCH_ASSOC)) {
-                /*
-                print "<pre>"; 
-                print_r($answer); 
-                print "</pre>"; 
-                */
+   if ($question["type"] == "MULTIPLE") {
+       
+       // display checkboxes buttons for answers to questions with MULTIPLE answers
+       while ($answer = $selectAnswers->fetch(PDO::FETCH_ASSOC)) {
+           /*
+           print "<pre>"; 
+           print_r($answer); 
+           print "</pre>"; 
+           */
 
-                // print html checkbox for each answer                    
-                echo '<div class="form-check">';                         
-                    echo '<input class="form-check-input" type="checkbox">'; 
-                    echo '<label class="form-check-label">' . $answer["answers"] . '</label><br>';         
-                echo '</div>';                                                     
-            }
-        } else {
-            // display radio buttons for answers to questions with one SINGLE answer
-            while ($answer = $selectAnswers->fetch(PDO::FETCH_ASSOC)) {
-                
-                /*
-                print "<pre>"; 
-                print_r($answer); 
-                print "</pre>"; 
-                */
-                
-                // print html radio button for each answer   
-                echo '<div class="form-check">';                                                  
-                    echo '<input class="form-check-input" type="radio">'; 
-                    echo '<label class="form-check-label">' . $answer["answers"] . '</label><br>';                                                                            
-                echo '</div>';                             
-            }    
-        }
-echo '<input type="hidden" class="form-control" id="questionNum" name="questionNum" value="<?php echo $quiz["questionNum"];" ?> 
+           // print html checkbox for each answer                    
+           echo '<div class="form-check">';                         
+               echo '<input class="form-check-input" type="checkbox">'; 
+               echo '<label class="form-check-label">' . $answer["answers"] . '</label><br>';         
+           echo '</div>';                                                     
+       }
+   } else {
+       // display radio buttons for answers to questions with one SINGLE answer
+       while ($answer = $selectAnswers->fetch(PDO::FETCH_ASSOC)) {
+           
+           /*
+           print "<pre>"; 
+           print_r($answer); 
+           print "</pre>"; 
+           */
+           
+           // print html radio button for each answer   
+           echo '<div class="form-check">';                                                  
+               echo '<input class="form-check-input" type="radio">'; 
+               echo '<label class="form-check-label">' . $answer["answers"] . '</label><br>';                                                                            
+           echo '</div>';                             
+       }    
+   } 
+
+    
+    ?> 
+  
+    
+
+
+</div>
+
+<div class="hidden">
+
+<input type="hidden" class="form-control" id="questionNum" name="questionNum" value="<?php echo $quiz["questionNum"]; ?>" >
 
 <input type="hidden" id="questLastInd" name="questLastInd" value="<?php echo $currentQuestionIndex; ?>">
 <input type="hidden" id="indexStep" name="indexStep" value="1">
 </div>
 
-<input class="btn btn-info bg-success text-white" type="submit" value="Submit">';
-    echo '</form>'; 
-echo '</div>';
-?>
-  <footer class="footer mt-auto py-3 bg-light">
-    <div class="container">
-      <span class="text-muted">Trivia Quiz</span>
-    </div>
-  </footer>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
-    </script>
-</body>
+<input class="btn btn-info" type="submit" value="Submit">           
 
+
+</form>
+
+
+  </div>
+
+  
+</main>
+
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container">
+
+  
+    <span class="text-muted">Trivia Quiz <?php echo $quiz["topic"] ?> Questions</span>
+  </div>
+</footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+      
+  </body>
 </html>
--- scripts/php_includes/connector.php
+++ scripts/php_includes/connector.php
@@ -13,16 +13,7 @@ try {
   // $dbConn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
   // set the PDO error mode to exception
   $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "<header>
-  <!-- Fixed navbar -->
-  <nav class='navbar navbar-expand-md navbar-dark fixed-top bg-dark'>
-    <div class='container-fluid'>
-      <h5 class='mt-0 text-light'>Trivia Quiz</h5>
-      <div class='collapse navbar-collapse' id='navbarCollapse'>
-      </div>
-    </div>
-  </nav>
-</header>";
+  echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
@@ -38,14 +29,14 @@ function fetchQuestionById($id, $dbConn) {
 
   return $row;
 };
-function fetchAnswerById($selectAnswers,$currentQuestionIndex, $dbConn) {
-  $answers = fetchAnswerById($selectAnswers,$currentQuestionIndex, $dbConn);
-  $selectAnswers = $dbConn->prepare("select * from answers where question_id = ?");
-  $selectAnswers->bindValue(1, $currentQuestionIndex["id"]);                 
-  $selectAnswers->execute(); 
+function fetchAnswerById($id, $dbConn) {
+
+  $sqlQuery=$dbConn->query("SELECT id,answers FROM `answers` WHERE `id` = $id");
+    $row = $sqlQuery->fetch(PDO::FETCH_ASSOC);
+
    // print_r($row);
 
-  return  $selectAnswers;
+  return $row;
 };
 
 function fetchQuestionIdSeq($topic, $questionNum, $dbConn) {
@@ -83,14 +74,10 @@ $nameMap = array(
   
 
 )
-
 );
-
 function nColumnName($columnName)
 {
   return NAME_MAP[$columnName];
-
 }
- // ucfirst "title" , "author" , "genre" , "description" , "publisher" , "price" , "currency" , "in_stock" , "ISBN"
 
 ?>
