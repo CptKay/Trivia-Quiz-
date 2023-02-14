d481a45932094d1c726b8ab449a7075e86baa8f5
julzdoug <jschwarzzh@hotmail.com>
2023-02-14 09:15:44 +0100

    question page image

--- questions.php
+++ questions.php
@@ -14,8 +14,8 @@ include "./scripts/php_includes/data-collector.php";
   <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/sticky-footer-navbar/">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-   <link rel="stylesheet" href="css/question.css">
-  
+  <link rel="stylesheet" href="css/question.css">
+
   <!-- Custom styles for this template -->
   <link href="css/sticky-footer-navbar.css" rel="stylesheet">
 </head>
@@ -46,29 +46,30 @@ include "./scripts/php_includes/data-collector.php";
   <br>
   <!-- Begin page content -->
   <div class="container mt-sm-5 my-1">
-    <main class="question ml-sm-5 pl-sm-5 pt-2">
+    <main class="question flex-shrink-0">
       <div class="py-2 h5"><b> Question
           <?php echo ($currentQuestionIndex + 1)
             ?> of
           <?php echo $quiz["questionNum"]; ?> :
           <?php echo $question["question_text"]; ?>
         </b></div>
-        
+      <?php echo '<img src="/images/' . $question["image"] . '" width="auto">'; ?>
       <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">Your answer:</h7>
-        <form style="width:auto;" class="form-select containerq"
-          onsubmit="return validateForm();" action="<?php echo $link; ?>" method="post">
+        <form style="width:auto;" class="form-select containerq" onsubmit="return validateForm();"
+          action="<?php echo $link; ?>" method="post">
           <?php
-          
+
           $correct = $answer["is_correct"] = 1;
           // prepare and execute the select statements
           $selectAnswers = $dbConn->prepare("select * from answers where question_id = ?");
           $selectAnswers->bindValue(1, $question["id"]);
           $selectAnswers->execute();
-          echo '<img src="/images' . $question["image"] . '" width="400px"></img>'; 
+
           if ($question["type"] == "MULTIPLE") {
             // display checkboxes buttons for answers to questions with MULTIPLE answers
             while ($answer = $selectAnswers->fetch(PDO::FETCH_ASSOC)) {
-              // print html checkbox for each answer                    
+              // print html checkbox for each answer  
+              echo '<div class="row justify-content-center">';
               echo '<div" class="form-check">';
               echo '<input class="form-check-input" type="checkbox">';
               echo '<label class="form-check-label">' . $answer["answers"] . '</label><br>';
@@ -85,23 +86,22 @@ include "./scripts/php_includes/data-collector.php";
             }
           }
           ?>
-      
-  <div class="d-flex align-items-center pt-3">
-        <div id="prev">
-  <div class="hidden">
-    <input type="hidden" class="form-control" id="questionNum" name="questionNum"
-      value="<?php echo $quiz["questionNum"]; ?>">
-    <input type="hidden" id="questLastInd" name="questLastInd" value="<?php echo $currentQuestionIndex; ?>">
-    <input type="hidden" id="indexStep" name="indexStep" value="1">
-    <input class="btn btn-info justify-content-center" type="submit" value="Submit">
-</div> 
-</div>
-  </div> 
+
+          <div class="d-flex align-items-center pt-3">
+            <div id="prev">
+              <div class="hidden">
+                <input type="hidden" class="form-control" id="questionNum" name="questionNum"
+                  value="<?php echo $quiz["questionNum"]; ?>">
+                <input type="hidden" id="questLastInd" name="questLastInd" value="<?php echo $currentQuestionIndex; ?>">
+                <input type="hidden" id="indexStep" name="indexStep" value="1">
+                <input class="btn btn-info justify-content-center" type="submit" value="Submit">
+              </div>
+            </div>
+          </div>
+      </div>
   </div>
-  </div>
-  </div>
-</form>
-     
+    </form>
+
   </main>
   <footer class="footer mt-auto py-3 bg-light">
     <div class="container">
