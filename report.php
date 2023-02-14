81794f1ce3f6109b9c75cc1d9bd7d23d36a8bd0c
julzdoug <jschwarzzh@hotmail.com>
2023-02-14 12:25:57 +0100

    report

--- report.php
+++ report.php
@@ -47,7 +47,7 @@
 $totalPoints = 0;
 
 foreach ($_SESSION as $name => $correct) {
-    if (str_contains($name, 'question-')) {
+    if (str_contains($name,  'question-')) {
         if (isset($correct["single-choice"])) {
             $points = intval($correct["single-choice"]);
             $totalPoints = $totalPoints + $points;
