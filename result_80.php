dbe5809f3e033bdc50ceb1bc7edca550a182a6eb
julzdoug <jschwarzzh@hotmail.com>
2023-02-15 23:22:39 +0100

    sound picture demo

--- result_80.php
+++ result_80.php
@@ -1,1 +1,4 @@
 <?php echo $totalPoints ?> of <?php echo $maxPoints ?> points!<br> or <?php echo $totalPoints_100 ?>%
+<?php echo "<audio autoplay>
+  <source src='/Sounds/applause.mp3' type='audio/mpeg'>
+</audio>" ?>
