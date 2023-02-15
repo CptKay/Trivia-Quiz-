86f5c1325b699b47bd9a553f50fd003c7d4b8a51
julzdoug <jschwarzzh@hotmail.com>
2023-02-15 12:25:56 +0100

    star page mobile ok

--- start.php
+++ start.php
@@ -1,44 +1,39 @@
 <?php
-   include "./scripts/php_includes/data-collector.php";
+include "./scripts/php_includes/data-collector.php";
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-<link rel="stylesheet" href="css/style.css">
-    <!-- Custom styles for this template -->
-
-      </head>
-  <body class="d-flex flex-column" style="background-color: #D7FDEC">
-<header>
-  <!-- Fixed navbar -->
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <div class="container-fluid">
-    <h5 class="mt-0 text-light">Trivia Quiz</h5>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+  <!-- Custom styles for this template -->
+  <title>QQA</title>
+</head>
+<body>
+  <header>
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div class="container-fluid">
+        <h5 class="mt-0 text-light">Trivia Quiz</h5>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+        </div>
       </div>
-    </div>
-  </nav>
-</header>
-    <!-- Begin page content -->
-<main class="flex-shrink-0">
+    </nav>
+  </header>
+  <!-- Begin page content -->
+  <div class="container d-flex">
+    <div class="col-md-12 text-center">
+      <img src="Icons/LOGOGO.png" class="rounded mx-auto d-block mt-5" alt="Quiz Starting image">
+         <a type="button" class="btn btn-primary btn-lg mt-0" href="index.php">Start</a>
+        </div>
+        </div>
+  <footer class="fixed-bottom mt-auto py-3 bg-light">
     <div class="container">
-<figure>
-  <img src="Icons/LOGOGO.png" class="rounded mx-auto d-block text-center mb-5 mt-5" alt="Quiz Starting image">
-</figure>
-<div class="col-md-12 text-center">
-            <a type="button" class="btn btn-primary btn-lg mt-0" href="index.php">Start</a>
-</div>
+      <span class="text-muted">Trivia Quiz</span>
     </div>
-</main>
-<footer class="fixed-bottom mt-auto py-3 bg-light">
-    <div class="container">
-    <span class="text-muted">Trivia Quiz</span>
-  </div>
-</footer>
+  </footer>
 </body>
-
 </html>
