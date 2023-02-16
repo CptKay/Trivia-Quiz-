346991d9b25e3bf79ff94d8c7b2bd6fcfb402a47
julzdoug <jschwarzzh@hotmail.com>
2023-02-16 12:02:13 +0100

    v.2

--- questions.php
+++ questions.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="css/style.css">
+  
   <!-- Custom styles for this template -->
   <title>QQA</title>
   <link rel="stylesheet" href="css/style.css">
@@ -32,18 +32,16 @@
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="index.php">
-          <h5 class="mt-0 text-light">Restart Trivia Quiz</h5>
+          <h5 class="mt-0 mb-0 text-light">Restart Trivia Quiz</h5>
         </a>
         <div class="collapse navbar-collapse" id="navbarCollapse">
         </div>
       </div>
-
     </nav>
   </header>
 
-  <br>
   <!-- Begin page content -->
-  <div class="container mt-2 mt-sm-5 my-1">
+  <div class="container mt-3 mt-sm-5 my-1">
     <main class="question flex-shrink-0">
       <div class="mt-2 py-2 h5"><b> Question
           <?php echo ($currentQuestionIndex + 1)
